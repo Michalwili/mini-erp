86250ab36640aef719120429a0a5b8ddcdc44e9c
michuwili <wilmowicz@gmail.com>
2026-07-04 13:09:55 +0200

    feat: dodaj repozytorium ProduktRepository

--- src/main/java/com/example/mini_erp/repository/ProduktRepository.java
+++ src/main/java/com/example/mini_erp/repository/ProduktRepository.java
@@ -1,4 +1,7 @@
 package com.example.mini_erp.repository;
 
-public class ProduktRepository {
+import com.example.mini_erp.model.Produkt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProduktRepository extends JpaRepository<Produkt, Long> {
 }
